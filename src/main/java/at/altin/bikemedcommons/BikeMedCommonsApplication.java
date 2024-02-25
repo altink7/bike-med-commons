fc4d7d678d2186d2231ff0dccd4b6f87cad50c21
altink7 <kelmendialtin48@gmail.com>
2024-02-25 21:04:13 +0100

    added dependency to maven for commons

---
+++ src/main/java/at/altin/bikemedcommons/BikeMedCommonsApplication.java
@@ -0,0 +1,12 @@
+package at.altin.bikemedcommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeMedCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeMedCommonsApplication.class, args);
+    }
+}
