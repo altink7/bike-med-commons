8e8c3d8ecfc1e64bfe293ab2442942c8533ee3ef
altink7 <kelmendialtin48@gmail.com>
2024-02-29 11:56:16 +0100

    release 1.1.0 queueConfig added

--- src/main/java/at/altin/bikemed/commons/config/QueueTestVariables.java
+++ src/main/java/at/altin/bikemed/commons/config/QueueTestVariables.java
@@ -1,16 +1,13 @@
 package at.altin.bikemed.commons.config;
 
-import lombok.Getter;
-
-@Getter
 public class QueueTestVariables {
 
     private QueueTestVariables() {
         // utility class
     }
 
-    private static final String HOSTNAME = "goose.rmq2.cloudamqp.com";
-    private static final String USERNAME = "juglmawp";
-    private static final String PASSWORD = "bg8I7Qo3zvto1wvrSksMJyRf56xuC7EX";
-    private static final String VIRTUAL_HOST = "juglmawp";
+    public static final String HOSTNAME = "goose.rmq2.cloudamqp.com";
+    public static final String USERNAME = "juglmawp";
+    public static final String PASSWORD = "bg8I7Qo3zvto1wvrSksMJyRf56xuC7EX";
+    public static final String VIRTUAL_HOST = "juglmawp";
 }
