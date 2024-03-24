2de712c537f2cd8b95fcda511f068fd770ee9239
altink7 <kelmendialtin48@gmail.com>
2024-03-24 13:58:27 +0100

    1.2.1 version updated

--- src/main/java/at/altin/bikemed/commons/config/QueueTestVariables.java
+++ src/main/java/at/altin/bikemed/commons/config/QueueTestVariables.java
@@ -7,7 +7,7 @@ public class QueueTestVariables {
     }
 
     public static final String HOSTNAME = "goose.rmq2.cloudamqp.com";
-    public static final String USERNAME = "juglmawp";
-    public static final String PASSWORD = "bg8I7Qo3zvto1wvrSksMJyRf56xuC7EX";
-    public static final String VIRTUAL_HOST = "juglmawp";
+    public static final String USERNAME = "otrxmisl";
+    public static final String PASSWORD = "xyaM60iHZe7B5dxwZYCAOEsahbWC19ay";
+    public static final String VIRTUAL_HOST = "otrxmisl";
 }
